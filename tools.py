cf7361dd13363bbb2c0c2be3c91dacaaeb5a33ed
Mohammadreza Amani <More.amani@yahoo.com>
2024-05-15 13:56:48 +0330

    initial django files

---
+++ tools.py
@@ -0,0 +1,67 @@
+from typing import List
+import json
+import asyncio
+import aiofiles
+import os
+
+PATH = "rest_framework"
+
+
+def source_to_list(code: str) -> List[str]:
+    """Convert a Python script to a list of lines."""
+    return [i + "\n" for i in code.split("\n")]
+
+
+def to_ipynb(code: str) -> dict:
+    """Convert a Python script to a Jupyter notebook."""
+
+    cells = []
+    for code_in_cell in code.split("\n\n\n"):
+        cells += [
+            {
+                "cell_type": "code",
+                "execution_count": None,
+                "metadata": {},
+                "outputs": [],
+                "source": source_to_list(code_in_cell),
+            },
+            {"cell_type": "markdown", "metadata": {}, "source": []},
+        ]
+    return {
+        "cells": cells,
+        "metadata": {"language_info": {"name": "python"}},
+        "nbformat": 4,
+        "nbformat_minor": 2,
+    }
+
+
+def load_code(path: str) -> str:
+    with open(path, "r", encoding="utf-8") as f:
+        return f.read()
+
+
+def to_json(cells: dict) -> str:
+    return json.dumps(cells, indent=4)
+
+
+async def write_ipynb(path: str, ipynb: str):
+    async with aiofiles.open(path, mode="w") as f:
+        await f.write(ipynb)
+
+
+async def do_conversion(path: str):
+    code = load_code(path)
+    cells = to_ipynb(code)
+    ipynb = to_json(cells)
+    await write_ipynb(path.replace(".py", ".ipynb"), ipynb)
+
+
+async def main():
+    # walk in files and folders of path and convert its python to ipynb format
+    for root, dirs, files in os.walk(PATH):
+        for file in files:
+            if file.endswith(".py"):
+                await do_conversion(os.path.join(root, file))
+
+
+asyncio.run(main())
